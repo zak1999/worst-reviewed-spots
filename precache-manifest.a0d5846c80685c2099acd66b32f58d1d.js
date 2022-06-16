c6bc567c1c12e46a550d466e71e6468871113459
zak1999 <zakariyya_ahmed@outlook.com>
2022-06-16 09:58:41 +0100

    Updates

---
+++ precache-manifest.a0d5846c80685c2099acd66b32f58d1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9941d9cfab3365b3ef2",
+    "url": "/static/css/main.b6dbc9db.chunk.css"
+  },
+  {
+    "revision": "e9941d9cfab3365b3ef2",
+    "url": "/static/js/main.e9941d9c.chunk.js"
+  },
+  {
+    "revision": "9330f2ea1885635384e0",
+    "url": "/static/css/1.202df1f9.chunk.css"
+  },
+  {
+    "revision": "9330f2ea1885635384e0",
+    "url": "/static/js/1.9330f2ea.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "454094cd20e3f1be896591dcccc4d1d1",
+    "url": "/index.html"
+  }
+];
